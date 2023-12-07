fd5665ed7893e8cb7905a876497be8e781288b0e
Marton Lajos <lajosmarton@hotmail.com>
2023-12-07 08:53:17 +0100

    Final version

---
+++ Stock/FileService.cs
@@ -0,0 +1,43 @@
+﻿namespace Stock;
+
+public class FileService
+{
+    private string filePath;
+
+    public FileService(string filePath)
+    {
+        this.filePath = filePath;
+    }
+
+    public List<Product> ParseDataFromCsv()
+    {
+        var lines = File.ReadAllLines(filePath);
+        var products = new List<Product>();
+        foreach (var line in lines.Skip(1))
+        {
+            var row = line.Replace(", ", ",");
+            var data = row.Split(',');
+
+            var newProduct = new Product();
+            newProduct.Id = int.Parse(data[0]);
+            newProduct.Name = data[1];
+            newProduct.Quantity = int.Parse(data[2]);
+            newProduct.Price = decimal.Parse(data[3].Replace(".", ","));
+            products.Add(newProduct);
+        }
+
+        return products;
+    }
+
+    public void WriteIntoFile(List<Product> allProducts)
+    {
+        var newRows = new List<string> { "Id,Name,Quantity,Price" };
+        foreach (var item in allProducts)
+        {
+            var newRow = $"{item.Id},{item.Name},{item.Quantity},{item.Price}";
+            newRows.Add(newRow);
+        }
+
+        File.WriteAllLines(filePath, newRows);
+    }
+}
--- Stock/Program.cs
+++ Stock/Program.cs
@@ -1,30 +1,12 @@
 ﻿using Stock;
 
 var filePath = "Phones.csv";
-var allProducts = ParseDataFromCsv(filePath);
+var fileService = new FileService(filePath);
+
+var allProducts = fileService.ParseDataFromCsv();
 var product = SearchProduct();
 SetNewPrice();
-WriteIntoFile(filePath);
-
-List<Product> ParseDataFromCsv(string filePath)
-{
-    var lines = File.ReadAllLines(filePath);
-    var products = new List<Product>();
-    foreach (var line in lines.Skip(1))
-    {
-        var row = line.Replace(", ", ",");
-        var data = line.Split(',');
-
-        var newProduct = new Product();
-        newProduct.Id = int.Parse(data[0]);
-        newProduct.Name = data[1];
-        newProduct.Quantity = int.Parse(data[2]);
-        newProduct.Price = decimal.Parse(data[3].Replace(".", ","));
-        products.Add(newProduct);
-    }
-
-    return products;
-}
+fileService.WriteIntoFile(allProducts);
 
 Product SearchProduct()
 {
@@ -68,16 +50,4 @@ void SetNewPrice()
 
         Console.WriteLine("Hibás formátumú új ár");
     } while (true);
-}
-
-void WriteIntoFile(string filePath)
-{
-    var newRows = new List<string> { "Id,Name,Quantity,Price" };
-    foreach (var item in allProducts)
-    {
-        var newRow = $"{item.Id},{item.Name},{item.Quantity},{item.Price}";
-        newRows.Add(newRow);
-    }
-
-    File.WriteAllLines(filePath, newRows);
 }
